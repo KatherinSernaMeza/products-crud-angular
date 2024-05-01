23b54fec13ad94afdaef24eea118e1f9d586d432
Katherin Serna <katherin.serna.m@gmail.com>
2024-05-01 05:22:32 +0000

    Set up custom color themes for application

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background))",
+        container: "rgba(var(--container))",
+        card: "rgba(var(--card))",
+        box: "rgba(var(--box))",
+        contBorder: "rgba(var(--contBorder))",
+        input: "rgba(var(--input))",
+        text: "rgba(var(--text))",
+        descriptiontext: "rgba(var(--descriptiontext))",
+        btn: "rgba(var(--btn))",
+        navlink: "rgba(var(--navlink))",
+        navtext: "rgba(var(--navtext))",
+      },
+    },
   },
   plugins: [],
 };
